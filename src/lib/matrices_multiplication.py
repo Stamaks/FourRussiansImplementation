aa492564ed0ee5391ca52fa9486b1717997a9d25
Alena Karnaukhova <st-mks@yandex.ru>
2021-03-21 16:56:06 +0300

    Added more tests, added loop to the main function

--- src/lib/matrices_multiplication.py
+++ src/lib/matrices_multiplication.py
@@ -9,22 +9,25 @@ def rounded_log(size_n):
     return math.ceil(math.log2(size_n))
 
 
-def precalculate(size_n):  # O(2^(log_n*2)*log_n) if not in the data folder
-    set_zero = set()
+def precalculate(vector_size):  # O(2^(vector_size*2)*vector_size) if not in the data folder
     set_one = set()
-    log_n = rounded_log(size_n)
-    if log_n <= 10 and os.path.exists(f'data/{log_n}'):
-        pass  # TODO: подкачка из файла
+    if vector_size <= 10 and os.path.exists(f'.data/{vector_size}'):
+        with open(f'.data/{vector_size}') as f:
+            set_one.update(map(int, f.readline().split(',')))
     else:
-        for i in range(2 ** log_n):
-            for j in range(i, 2 ** log_n):
+        for i in range(2 ** vector_size):
+            for j in range(i, 2 ** vector_size):
                 bit_and = i & j  # O(1)
-                if bin(bit_and).count('1') % 2:  # O(log_n)
+                if bin(bit_and).count('1') % 2:  # O(vector_size)
                     set_one.add(bit_and)  # O(1)
-                else:
-                    set_zero.add(bit_and)  # O(1)
 
-    return set_zero, set_one
+        if not os.path.exists('.data'):
+            os.mkdir('.data')
+
+        with open(f'.data/{vector_size}', 'w') as f:
+            f.write(','.join(map(str, set_one)))
+
+    return set_one
 
 
 def get_cmprsd_matr_size(size_n, vector_size):
@@ -43,8 +46,6 @@ def get_compressed_matrix(matrix, size_n, vector_size, is_first_matrix=True):
         for cmprsd_ind, j in enumerate(range(0, size_n, vector_size)):  # from 0 to n, step log_n
             number = 0
             degree = 0
-            # if j + vector_size > size_n:
-            #     degree = j + vector_size - size_n
             for vec_ind in range(min(j + vector_size - 1, size_n - 1), j - 1, -1):
                 if is_first_matrix:
                     number += matrix[i][vec_ind] * 2 ** degree
@@ -64,9 +65,8 @@ def run_four_russians(matrix_a, matrix_b, size_n):
     if size_n == 1:
         return [[matrix_a[0][0] * matrix_b[0][0]]]
 
-    set_zero, set_one = precalculate(size_n)
-
     log_n = rounded_log(size_n)
+    set_one = precalculate(size_n)
 
     matrix_c = [[0] * size_n for i in range(size_n)]
 
@@ -77,7 +77,6 @@ def run_four_russians(matrix_a, matrix_b, size_n):
         for j in range(size_n):
             mult_sum = 0
             for k in range(get_cmprsd_matr_size(size_n, log_n)):
-                mult = cmprsd_matrix_a[i][k] & cmprsd_matrix_b[k][j]
                 mult_sum += int(cmprsd_matrix_a[i][k] & cmprsd_matrix_b[k][j] in set_one)
             matrix_c[i][j] = mult_sum % 2
 
--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
 from lib.matrices_multiplication import run_four_russians
 
 
+# TODO: Add README
+# TODO: проверить на асимптотику
+
+
 def get_matrix(size_n):
     matrix = [list(map(int, input().split())) for _ in range(size_n)]
     check_boolean = all(i == 0 or i == 1 for row in matrix for i in row)
@@ -17,36 +21,48 @@ if __name__ == '__main__':
     print('Welcome to the 4 Russians algorithm implementation!')
     print('NOTE: The algorithm works only with boolean square matrices!')
 
-    size_n = 0
-    while size_n <= 0:
-        print('Each matrix size is n x n, enter n (just one integer number):')
-        try:
-            size_n = int(input())
-        except Exception:
-            pass
+    while(True):
+        size_n = 0
+        while size_n <= 0:
+            print('Each matrix size is n x n, enter n (just one integer number):')
+            try:
+                size_n = int(input())
+            except Exception:
+                pass
 
-    matrix_a = None
-    while not matrix_a:
-        print(f'Please, enter the first boolean matrix, size {size_n} x {size_n}:')
-        try:
-            matrix_a = get_matrix(size_n)
-        except Exception:
-            pass
+        matrix_a = None
+        while not matrix_a:
+            print(f'Please, enter the first boolean matrix, size {size_n} x {size_n}:')
+            try:
+                matrix_a = get_matrix(size_n)
+            except Exception:
+                pass
 
-    print('The first matrix is:')
-    print_matrix(matrix_a)
+        print('The first matrix is:')
+        print_matrix(matrix_a)
 
-    matrix_b = None
-    while not matrix_b:
-        print(f'Enter the second boolean matrix, size {size_n} x {size_n}:')
-        try:
-            matrix_b = get_matrix(size_n)
-        except Exception:
-            pass
+        matrix_b = None
+        while not matrix_b:
+            print(f'Enter the second boolean matrix, size {size_n} x {size_n}:')
+            try:
+                matrix_b = get_matrix(size_n)
+            except Exception:
+                pass
 
-    print('The second matrix is:')
-    print_matrix(matrix_b)
+        print('The second matrix is:')
+        print_matrix(matrix_b)
 
-    final_matrix = run_four_russians(matrix_a, matrix_b, size_n)
-    print('The final matrix is:')
-    print_matrix(final_matrix)
+        final_matrix = run_four_russians(matrix_a, matrix_b, size_n)
+        print('The final matrix is:')
+        print_matrix(final_matrix)
+
+        answer = None
+        while answer != 'y':
+            print('Want to run the algorithm again? Enter y/n')
+            try:
+                answer = input()
+                if answer == 'n':
+                    exit(0)
+            except Exception:
+                pass
+
